0c4e9c17e28e39c33b2828db909328bb5eb5c923
Bruno Meireles <brunobritomeireles@gmail.com>
2019-02-10 12:18:05 -0300

    Adicionado tratamento de erros em categorias.

--- adiciona-categoria.php
+++ adiciona-categoria.php
@@ -2,14 +2,18 @@
     require_once 'cabecalho.php';
 
     verificaUsuario();
+    try{
+        $categoria = new Categoria();
+        
+        $categoria->setNome($_POST['nome']);
+        if ( $categoria->inserir() ) {
+            $_SESSION['success'] = "Categoria '".$categoria->getNome()."' adicionada com sucesso.";
+        } else {
+            $_SESSION['danger'] = "Erro: Erro ao inserir categoria.";
+        }
 
-    $categoria = new Categoria();
-    
-    $categoria->setNome($_POST['nome']);
-    if ( $categoria->inserir() ) {
-        $_SESSION['success'] = "Categoria '".$categoria->getNome()."' adicionada com sucesso.";
-    } else {
-        $_SESSION['danger'] = "Erro: Erro ao inserir categoria.";
+        header('Location: categoria-lista.php');
+    }catch(Exception $e){
+        Erro::trataErro($e);
     }
-
-    header('Location: categoria-lista.php');
+?>
